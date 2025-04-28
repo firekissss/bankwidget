385bc90f3341d066115c00f4c50222420b73ff95
Артём Лабунский <darkchrom74@gmail.com>
2025-04-28 23:57:10 +0300

    add protection of no-digit input to mask_account_card

--- src/widget.py
+++ src/widget.py
@@ -2,8 +2,11 @@ import re
 import masks
 
 def mask_account_card(card_info: str) -> str:
+    first_digit_match = re.search(r'\d', card_info)
+    if not first_digit_match:
+        raise ValueError("Не найдены цифры в строке")
+    first_digit_index = first_digit_match.start()
     if card_info[:4] == "Счет":
         return card_info[:5] + masks.get_mask_account(card_info[5:])
     else:
-        first_digit: int = re.search(r'\d', card_info).start()
-        return card_info[:first_digit] + masks.get_mask_card_number(card_info[first_digit:])
+        return card_info[:first_digit_index] + masks.get_mask_card_number(card_info[first_digit_index:])
