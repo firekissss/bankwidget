b3a824d9d17b3c6eadccbaf9b5fc5a00a39dce2e
Артём Лабунский <darkchrom74@gmail.com>
2025-04-30 19:07:25 +0300

    add sort_by_date function

--- src/processing.py
+++ src/processing.py
@@ -1,6 +1,15 @@
-def filter_by_state(dictionary_input_list: list, state:str = 'EXECUTED') -> list:
+from datetime import datetime
+
+def filter_by_state(dictionary_input_list: list[dict], state:str = 'EXECUTED') -> list[dict]:
     output_dictionary_list: list = []
     for item in dictionary_input_list:
         if item['state'] == state:
             output_dictionary_list.append(item)
     return output_dictionary_list
+
+def sort_by_date(dictionary_input_list: list[dict], order: str = 'descending') -> list[dict]:
+    return sorted(
+        dictionary_input_list,
+        key=lambda x: datetime.fromisoformat(x['date']),
+        reverse=True if order == 'descending' else False
+    )
