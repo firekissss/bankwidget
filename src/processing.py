bae76c92c1d8a6fcd4653f9b65a2d881d2ee1f7a
Артём Лабунский <darkchrom74@gmail.com>
2025-04-30 20:59:05 +0300

    fixed the parameter of sort_by_date function and related with it README.md example

--- src/processing.py
+++ src/processing.py
@@ -11,17 +11,13 @@ def filter_by_state(dictionary_input_list: list[dict], state: str = "EXECUTED") 
     return output_dictionary_list
 
 
-def sort_by_date(dictionary_input_list: list[dict], order: str = "descending") -> list[dict]:
+def sort_by_date(dictionary_input_list: list[dict], is_reversed: bool = True) -> list[dict]:
     """sorts an input dictionary list by key 'date' and value in ISO format
-    in descending (default) or ascending order"""
-    formatted_order = order.lower()
-    if formatted_order not in ("ascending", "descending"):
-        raise ValueError("Аргумент 'order' может быть либо 'ascending', либо 'descending'.")
-
+    in descending (default, True) or ascending (False) order"""
     return sorted(
         dictionary_input_list,
         key=lambda x: datetime.fromisoformat(x["date"]),
-        reverse=True if formatted_order == "descending" else False,
+        reverse=is_reversed,
     )
     # Не использую функцию get_date из модуля widget, т.к. она возвращает только дату
     # в таком случае, операции за один и тот же день могут неверно сортироваться из-за неучёта времени
