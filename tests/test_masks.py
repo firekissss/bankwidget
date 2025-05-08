36bb409bdcdc292044a198a603f5d63ba9015119
Артём Лабунский <darkchrom74@gmail.com>
2025-05-09 01:43:29 +0300

    added tests to get_mask_account, re-wrote tests for get_mask_card_number

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,32 +1,54 @@
 import pytest
 
-from src import masks
+from src.masks import *
 
 
 # testing get_mask_card_number
 
 def test_get_mask_card_number_correct(correct_card_number):
-    assert masks.get_mask_card_number(correct_card_number) == "1234 56** **** 1121"
+    assert get_mask_card_number(correct_card_number) == "1234 56** **** 1121"
 
 
-@pytest.mark.parametrize("card_number", [
-    "12345678901234567890",
-    "1234",
+@pytest.mark.parametrize("invalid_value, wanted_exc_info", [
+    ("", "Номер карты должен состоять только из цифр"),
+    ("123mixedstring", "Номер карты должен состоять только из цифр"),
+    ("correctlengthstr", "Номер карты должен состоять только из цифр"),
+
+    ("12345678901234567", "Номер карты должен содержать 16 цифр"),
+    ("12345678901234567890", "Номер карты должен содержать 16 цифр"),
+    ("123456789012345", "Номер карты должен содержать 16 цифр"),
+    ("1234", "Номер карты должен содержать 16 цифр")
 ])
-def test_get_mask_card_number_incorrect_length(card_number):
+def test_get_mask_card_number_any_invalid_input(invalid_value, wanted_exc_info):
     with pytest.raises(ValueError) as result_exc_info:
-        masks.get_mask_card_number(card_number)
+        get_mask_card_number(invalid_value)
 
-    assert str(result_exc_info.value) == "Номер карты должен содержать 16 цифр"
+    assert str(result_exc_info.value) == wanted_exc_info
 
 
-@pytest.mark.parametrize("some_string", [
-    "",
-    "123mixedstring",
-    "correctlengthstr"
+# testing get_mask_account
+
+@pytest.mark.parametrize("account_number, masked_number", [
+    ("123456", "**3456"),
+    ("12345678901234567890", "**7890")
 ])
-def test_get_mask_card_number_no_number(some_string):
-    with pytest.raises(ValueError) as result_exc_info:
-        masks.get_mask_card_number(some_string)
+def test_get_mask_account_correct_number(account_number, masked_number):
+    assert get_mask_account(account_number) == masked_number
 
-    assert str(result_exc_info.value) == "Номер карты должен состоять только из цифр"
+
+@pytest.mark.parametrize("invalid_account_number, wanted_exc_info", [
+    ("1234text5678", "Номер счёта должен состоять только из цифр"),
+    ("", "Номер счёта должен состоять только из цифр"),
+    ("onlytext", "Номер счёта должен состоять только из цифр"),
+    (" ", "Номер счёта должен состоять только из цифр"),
+    ("anything 123_456?% with space", "Номер счёта должен состоять только из цифр"),
+
+    ("123", "Номер счёта слишком короткий"),
+    ("12", "Номер счёта слишком короткий"),
+    ("1", "Номер счёта слишком короткий")
+])
+def test_get_mask_account_any_invalid_input(invalid_account_number, wanted_exc_info):
+    with pytest.raises(ValueError) as result_exc_info:
+        get_mask_account(invalid_account_number)
+
+    assert str(result_exc_info.value) == wanted_exc_info
