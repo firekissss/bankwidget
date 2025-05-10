6bc36df557f4b25d94a486a6afd03b5ce36df1ea
Артём Лабунский <darkchrom74@gmail.com>
2025-05-10 16:58:36 +0300

    commit everything, smth appeared to be uncommitted

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,18 +4,3 @@ import pytest
 @pytest.fixture
 def correct_card_number():
     return "1234567891011121"
-
-
-# @pytest.fixture
-# def longer_card_number():
-#     return "12345678901234567890"
-#
-#
-# @pytest.fixture
-# def shorter_card_number():
-#     return "1234"
-#
-#
-# @pytest.fixture
-# def zero_length_card_number():
-#     return ""
